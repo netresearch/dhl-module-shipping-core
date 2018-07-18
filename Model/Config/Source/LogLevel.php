38dee7725a019c64ef6858fb408c8d269ce1a0e1
Ronny Gertler <ronny.gertler@netresearch.de>
2018-07-18 13:45:23 +0200

    DHLGW-24: Rename DebugLog to Loglevel, move Product models to carrier module

--- Model/Config/Source/LogLevel.php
+++ Model/Config/Source/LogLevel.php
@@ -7,14 +7,14 @@
 namespace Dhl\ShippingCore\Model\Config\Source;
 
 /**
- * Class InternationalDefaultProduct
+ * Class DebugLog
  *
  * @package Dhl\ShippingCore\Model\Backend\Config\Source
  * @author Ronny Gertler <ronny.gertler@netresearch.de>
  * @copyright 2018 Netresearch GmbH & Co. KG
  * @link http://www.netresearch.de/
  */
-class InternationalDefaultProduct implements \Magento\Framework\Option\ArrayInterface
+class LogLevel implements \Magento\Framework\Option\ArrayInterface
 {
     /**
      * {@inheritdoc}
@@ -22,11 +22,9 @@ class InternationalDefaultProduct implements \Magento\Framework\Option\ArrayInte
     public function toOptionArray()
     {
         return [
-            ['value' => 'XPD', 'label' =>  'EXPRESS ENVELOPE'],
-            ['value' => 'DOX', 'label' =>  'EXPRESS WORLDWIDE'],
-            ['value' => 'TDK', 'label' =>  'EXPRESS 9:00'],
-            ['value' => 'TDL', 'label' =>  'EXPRESS 10:30'],
-            ['value' => 'TDT', 'label' =>  'EXPRESS 12:00']
+            ['value' => 'ERROR', 'label' =>  'Error'],
+            ['value' => 'INFO', 'label' =>  'Info'],
+            ['value' => 'DEBUG', 'label' =>  'Debug']
         ];
     }
 }
