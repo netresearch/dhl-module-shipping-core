45bb38e6fbf725c0a5e0abe9befd312a54d520db
Paul Siedler <paul.siedler@netresearch.de>
2018-07-20 12:29:08 +0200

    DHLGW-25: move payment method source model to common source model namespace

--- Model/Config/Source/AllPaymentMethods.php
+++ Model/Config/Source/AllPaymentMethods.php
@@ -3,10 +3,10 @@
  * See LICENSE.md for license details.
  */
 
-namespace Dhl\ShippingCore\Model\Payment\Config\Source;
+namespace Dhl\ShippingCore\Model\Config\Source;
 
 use Magento\Framework\Data\OptionSourceInterface;
-use Magento\Payment\Helper\Data;
+use Magento\Payment\Helper\Data as PaymentHelper;
 
 /**
  * Payment methods source model.
@@ -19,22 +19,22 @@ use Magento\Payment\Helper\Data;
  * @link https://github.com/magento/magento2/issues/13460
  * @link https://bugs.nr/DHLVM2-197
  *
- * @package  Dhl\Shipping\Model
+ * @package  Dhl\ShippingCore\Model\Config\Source
  * @author   Ronny gertler <ronny.gertler@netresearch.de>
  * @license  http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  * @link     http://www.netresearch.de/
  */
-class Allmethods implements OptionSourceInterface
+class AllPaymentMethods implements OptionSourceInterface
 {
     /**
-     * @var Data
+     * @var PaymentHelper
      */
     private $paymentData;
 
     /**
-     * @param Data $paymentData
+     * @param PaymentHelper $paymentData
      */
-    public function __construct(Data $paymentData)
+    public function __construct(PaymentHelper $paymentData)
     {
         $this->paymentData = $paymentData;
     }
