afce83b8cb73c2e137718c44eb7089a5c90f0c2c
Paul Siedler <paul.siedler@netresearch.de>
2018-08-02 11:04:58 +0200

    DHLGW-39: provide baseline configuration for packaging popup with stubs for data provider

--- DataProvider/PackagingPopup.php
+++ DataProvider/PackagingPopup.php
@@ -8,23 +8,59 @@ namespace Dhl\ShippingCore\DataProvider;
 
 use Magento\Ui\DataProvider\AbstractDataProvider;
 
+/**
+ * Class PackagingPopup
+ *
+ * @package Dhl\ShippingCore\DataProvider
+ * @author Paul Siedler <paul.siedler@netresearch.de>
+ * @copyright 2018 Netresearch GmbH & Co. KG
+ * @link http://www.netresearch.de/
+ */
 class PackagingPopup extends AbstractDataProvider
 {
     public function getData(): array
     {
-        return [ 'items' => [
+        return [
+            'service' => $this->getServiceInputs(),
+            'package' => array_merge($this->getPackageInputs(), $this->getExportPackageInputs()),
+            'items' => array_merge($this->getItemInputs(), $this->getExportItemInputs()),
+        ];
+    }
+
+    private function getServiceInputs(): array
+    {
+        /** @TODO: use provider classes for input retrieval */
+        return [
             'Hello',
             'Darkness',
             'my',
             'old',
-            'friend'
-            ],
+            'friend',
         ];
     }
 
-    public function addFilter(\Magento\Framework\Api\Filter $filter)
+    private function getPackageInputs(): array
     {
-        return;
+        /** @TODO provide default package inputs */
+        return [];
+    }
+
+    private function getExportPackageInputs(): array
+    {
+        /** @TODO: use provider classes for input retrieval */
+        return [];
+    }
+
+    private function getItemInputs(): array
+    {
+        /** @TODO: provide default inputs */
+        return [];
+    }
+
+    private function getExportItemInputs(): array
+    {
+        /** @TODO: use provider classes for input retrieval */
+        return [];
     }
 
 }
--- view/adminhtml/web/js/service-fieldset.js
+++ view/adminhtml/web/js/service-fieldset.js
@@ -1,10 +1,10 @@
 define([
-        "Magento_Ui/js/form/form"
+        "Magento_Ui/js/form/components/fieldset"
     ], function (Component) {
         return Component.extend({
-            /** @TODO: handle form flow, provide save callback etc. */
             initialize: function () {
                 this._super();
+                /** @TODO: initialize fields from datasource */
             }
         });
     }
