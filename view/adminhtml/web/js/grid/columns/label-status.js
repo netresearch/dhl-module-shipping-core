74c387a362382965e8ed78250d8590be3cdd7646
Sebastian Ertner <sebastian.ertner@netresearch.de>
2019-03-14 09:57:45 +0100

    DHLGW-243: add label status column

---
+++ view/adminhtml/web/js/grid/columns/label-status.js
@@ -0,0 +1,14 @@
+/**
+ * See LICENSE.md for license details.
+ */
+define([
+    'jquery',
+    'ko',
+    'Magento_Ui/js/grid/columns/column'
+], function ($, ko, Element) {
+    'use strict';
+
+    return Element.extend({
+
+    });
+});
