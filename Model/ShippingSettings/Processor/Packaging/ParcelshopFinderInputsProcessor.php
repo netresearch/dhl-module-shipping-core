662239fc9f5c6ead58238235991b3be35922f671
Max Melzer <max.melzer@netresearch.de>
2019-10-24 16:45:30 +0200

    DHLGW-626: Add Processor to add virtual parcelshop finder inputs

---
+++ Model/ShippingSettings/Processor/Packaging/ParcelshopFinderInputsProcessor.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * See LICENSE.md for license details.
+ */
+declare(strict_types=1);
+
+namespace Dhl\ShippingCore\Model\ShippingSettings\Processor\Packaging;
+
+use Dhl\ShippingCore\Api\Data\ShippingSettings\ShippingOption\InputInterface;
+use Dhl\ShippingCore\Api\Data\ShippingSettings\ShippingOption\InputInterfaceFactory;
+use Dhl\ShippingCore\Api\Data\ShippingSettings\ShippingOptionInterface;
+use Dhl\ShippingCore\Api\ShippingSettings\Processor\Packaging\ShippingOptionsProcessorInterface;
+use Magento\Sales\Api\Data\ShipmentInterface;
+
+/**
+ * Class ParcelshopFinderInputsProcessor
+ *
+ * @author Max Melzer <max.melzer@netresearch.de>
+ */
+class ParcelshopFinderInputsProcessor implements ShippingOptionsProcessorInterface
+{
+    /**
+     * The input type for the shopfinder component.
+     */
+    const INPUT_TYPE_SHOPFINDER = 'shopfinder';
+
+    /*
+     * The virtual input's codes that will be added
+     * to the shipping option with the shopfinder input.
+     */
+    const INPUT_CODES = [
+        'shop-name',
+        'address-street',
+        'address-postalcode',
+        'address-city',
+        'address-country',
+    ];
+
+    /**
+     * @var InputInterfaceFactory
+     */
+    private $inputFactory;
+
+    /**
+     * ParcelshopFinderInputsProcessor constructor.
+     *
+     * @param InputInterfaceFactory $inputFactory
+     */
+    public function __construct(InputInterfaceFactory $inputFactory)
+    {
+        $this->inputFactory = $inputFactory;
+    }
+
+    /**
+     * @param ShippingOptionInterface[] $optionsData
+     * @param ShipmentInterface $shipment
+     * @return ShippingOptionInterface[]
+     */
+    public function process(
+        array $optionsData,
+        ShipmentInterface $shipment
+    ): array {
+        $index = null;
+        $shopFinder = null;
+        foreach ($optionsData as $key => $option) {
+            foreach ($option->getInputs() as $input) {
+                if ($input->getInputType() === self::INPUT_TYPE_SHOPFINDER) {
+                    $index = $key;
+                    $shopFinder = $option;
+                    break 2;
+                }
+            }
+        }
+
+        if ($shopFinder && $index) {
+            $inputs = $shopFinder->getInputs();
+            foreach (self::INPUT_CODES as $inputCode) {
+                /** @var InputInterface $input */
+                $input = $this->inputFactory->create();
+                $input->setCode($inputCode);
+                $input->setInputType('static');
+                $inputs[$inputCode] = $input;
+            }
+            $shopFinder->setInputs($inputs);
+            $optionsData[$index] = $shopFinder;
+        }
+
+        return $optionsData;
+    }
+}
