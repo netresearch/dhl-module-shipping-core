e5a07155510a637b4988398b85d0554c97a6ba6d
Paul Siedler <paul.siedler@netresearch.de>
2020-04-24 02:12:44 +0200

    DHLGW-513: implement runtime cache for shipment specific shipment settings

--- Model/ShippingSettings/PackagingDataProvider.php
+++ Model/ShippingSettings/PackagingDataProvider.php
@@ -6,22 +6,17 @@ declare(strict_types=1);
 
 namespace Dhl\ShippingCore\Model\ShippingSettings;
 
+use Dhl\ShippingCore\Api\Data\ShippingSettings\CarrierDataInterface;
 use Dhl\ShippingCore\Api\Data\ShippingSettings\ShippingDataInterface;
 use Dhl\ShippingCore\Model\ShippingSettings\Processor\Packaging\ArrayProcessor\PackagingArrayCompositeProcessor;
 use Dhl\ShippingCore\Model\ShippingSettings\Processor\Packaging\PackagingDataCompositeProcessor;
 use Magento\Framework\Config\ReaderInterface;
 use Magento\Sales\Api\Data\ShipmentInterface;
 
-/**
- * Class PackagingDataProvider
- *
- * @author  Max Melzer <max.melzer@netresearch.de>
- * @link    https://www.netresearch.de/
- */
 class PackagingDataProvider
 {
     const GROUP_PACKAGE = 'packageOptions';
-    const GROUP_ITEM    = 'itemOptions';
+    const GROUP_ITEM = 'itemOptions';
     const GROUP_SERVICE = 'serviceOptions';
 
     /**
@@ -44,6 +39,19 @@ class PackagingDataProvider
      */
     private $shippingDataHydrator;
 
+    /**
+     * @var ShippingDataInterface[]
+     */
+    private $shipmentData = [];
+
+    /**
+     * PackagingDataProvider constructor.
+     *
+     * @param ReaderInterface $reader
+     * @param PackagingArrayCompositeProcessor $compositeArrayProcessor
+     * @param PackagingDataCompositeProcessor $compositeDataProcessor
+     * @param ShippingDataHydrator $shippingDataHydrator
+     */
     public function __construct(
         ReaderInterface $reader,
         PackagingArrayCompositeProcessor $compositeArrayProcessor,
@@ -64,10 +72,21 @@ class PackagingDataProvider
      */
     public function getData(ShipmentInterface $shipment): ShippingDataInterface
     {
+        if (!empty($shipment->getEntityId()) && isset($this->shipmentData[$shipment->getEntityId()])) {
+            // use cached packaging data
+            return $this->shipmentData[$shipment->getEntityId()];
+        }
+
         $packagingDataArray = $this->reader->read('adminhtml');
         $packagingDataArray = $this->compositeArrayProcessor->process($packagingDataArray, $shipment);
+
         $packagingData = $this->shippingDataHydrator->toObject($packagingDataArray);
         $packagingData = $this->compositeDataProcessor->process($packagingData, $shipment);
+
+        if (!empty($shipment->getEntityId())) {
+            // cache packaging data if shipment has an identifier
+            $this->shipmentData[$shipment->getEntityId()] = $packagingData;
+        }
 
         return $packagingData;
     }
