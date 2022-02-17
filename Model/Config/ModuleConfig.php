2c6777a8320e885b81e42f15bbbee4f0d8bbe06f
Christoph Aßmann <christoph.assmann@netresearch.de>
2022-02-17 10:55:03 +0100

    DHLGW-1195: display metapackage version number in config infobox

--- Model/Config/ModuleConfig.php
+++ Model/Config/ModuleConfig.php
@@ -8,30 +8,35 @@ declare(strict_types=1);
 
 namespace Dhl\ShippingCore\Model\Config;
 
-use Magento\Framework\App\Config\ScopeConfigInterface;
 use Netresearch\ShippingCore\Api\InfoBox\VersionInterface;
 
 class ModuleConfig implements VersionInterface
 {
-    public const CONFIG_PATH_VERSION = 'dhlshippingsolutions/version';
+    private const METAPACKAGE_NAME = 'dhl/shipping-m2';
 
     public const CONFIG_PATH_TERMS_OF_TRADE = 'dhlshippingsolutions/dhlglobalwebservices/shipment_defaults/terms_of_trade';
 
     /**
-     * @var ScopeConfigInterface
-     */
-    private $scopeConfig;
-
-    public function __construct(ScopeConfigInterface $scopeConfig)
-    {
-        $this->scopeConfig = $scopeConfig;
-    }
-
-    /**
+     * Obtain the DHL Shipping metapackage version number.
+     *
+     * Since this package does not provide much more than the Post & DHL
+     * configuration section, there are not many changes to be expected
+     * and the package's own version number is of no interest to anyone.
+     * Hence, we use the General Configuration header to display the
+     * metapackage version if available.
+     *
      * @return string
      */
     public function getModuleVersion(): string
     {
-        return $this->scopeConfig->getValue(self::CONFIG_PATH_VERSION);
+        if (!class_exists('\Composer\InstalledVersions')) {
+            return '';
+        }
+
+        try {
+            return \Composer\InstalledVersions::getPrettyVersion(self::METAPACKAGE_NAME);
+        } catch (\OutOfBoundsException $exception) {
+            return '';
+        }
     }
 }
